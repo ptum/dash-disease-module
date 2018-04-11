d9fc0f13b11c979a6312d2fb62d07711d36ebfa2
ptum <schoobdar@gmail.com>
2018-04-11 14:56:19 +0200

    About section

--- apps/contactus.py
+++ apps/contactus.py
@@ -23,7 +23,7 @@ layout = html.Div([  # page 6
                 html.H6('About',
                         className="gs-header gs-text-header padded"),
                 html.Br([]),
-                html.P(children=["A web app and library for visualising disease associated modules from",
+                html.P(children=["A web app and library for visualising disease associated modules from ",
                             html.A(
                                 children='DREAM challenge for disease modules identification',
                                 target= '_blank',
@@ -35,7 +35,7 @@ layout = html.Div([  # page 6
                 html.H6('Contact information:',
                         className="gs-header gs-text-header padded"),
                 html.Br([]),
-                html.P("If you have questions, comments or suggestions, we'd like to hear them."),
+                html.P("If you have questions, comments or suggestions, we'd like to hear them @"),
                 html.P('Sarvenaz Choobdar(schoob...@gmail.com)'),
             ], className="row"),
             # Row 3
@@ -43,14 +43,15 @@ layout = html.Div([  # page 6
                 html.H6('Cite:',
                         className="gs-header gs-text-header padded"),
                 html.Br([]),
-                html.P("Challenge preprint:"),
-                html.P("Open Community Challenge Reveals Molecular Network Modules with Key Roles in Diseases"),
-                htm.P("Sarvenaz Choobdar, Mehmet E. Ahsen, Jake Crawford, Mattia Tomasoni, David Lamparter, \
-                    Junyuan Lin, Benjamin Hescott, Xiaozhe Hu, Johnathan Mercer, Ted Natoli, Rajiv Narayan, \
-                    The DREAM Module Identification Challenge Consortium, Aravind Subramanian, \
-                    Gustavo Stolovitzky, Zoltán Kutalik, Kasper Lage, Donna K. Slonim, Julio Saez-Rodriguez, \
-                    Lenore J. Cowen, Sven Bergmann, Daniel Marbach."),
-                html.A(children ='bioRxiv 265553 (2018).', target='_blank', href='https://www.biorxiv.org/content/early/2018/02/15/265553'),
+                html.P("Challenge preprint:",style = {'fontWeight':600}),
+                html.P(children =["- Open Community Challenge Reveals Molecular Network Modules with Key Roles in Diseases",
+                                    html.Br([]),"Sarvenaz Choobdar, Mehmet E. Ahsen, Jake Crawford, Mattia Tomasoni, David Lamparter, \
+                                        Junyuan Lin, Benjamin Hescott, Xiaozhe Hu, Johnathan Mercer, Ted Natoli, Rajiv Narayan, \
+                                        The DREAM Module Identification Challenge Consortium, Aravind Subramanian, \
+                                        Gustavo Stolovitzky, Zoltan Kutalik, Kasper Lage, Donna K. Slonim, Julio Saez-Rodriguez, \
+                                        Lenore J. Cowen, Sven Bergmann, Daniel Marbach.",html.Br([]),
+                                 html.A(children ='bioRxiv 265553 (2018).', target='_blank', href='https://www.biorxiv.org/content/early/2018/02/15/265553')
+                        ]),
             ], className="row"),
 
         ], className="subpage")
